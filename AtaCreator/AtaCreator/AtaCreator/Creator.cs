326ce86176a235a70fd13d6a4d4e3acb6876e3f3
Lucas Ballestero <79941119+LukeBalles@users.noreply.github.com>
2022-04-03 14:22:28 -0300

    Sixth commit

---
+++ AtaCreator/AtaCreator/AtaCreator/Creator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AtaCreator
+{
+    internal class Creator
+    {
+        public ArrayList names = new ArrayList();
+        public string title;
+        public int numbPart = 1;
+        public string description;
+        public string text;
+        int i = 1;
+        public void Cre()
+        {
+            
+
+            using (StreamWriter sw = new StreamWriter(text))
+            {
+                var date = DateTime.Now;
+                sw.WriteLine($"Title of meeting: {title}");
+                sw.WriteLine();
+                sw.WriteLine($"Description: {description}");
+                sw.WriteLine();
+                sw.WriteLine($"Number of participants: {numbPart}");
+                sw.WriteLine();
+                foreach (string name in names)
+                {  
+                    sw.WriteLine($"{i}º Participant: {name}");
+                    i++;
+                }
+                sw.WriteLine();
+                sw.WriteLine($"Date: {date}");
+                Console.WriteLine("The Ata was created!");
+            }
+        }
+    }
+}
